978b45733d644e996bb62f8c955795aa9a61346a
nayna096 <nayna096@uottawa.ca>
2022-11-04 23:41:06 -0400

    Abstract Class Fix

--- app/src/main/java/com/example/meallogin/Administrator.java
+++ app/src/main/java/com/example/meallogin/Administrator.java
@@ -1,14 +1,15 @@
 package com.example.meallogin;
 
 public class Administrator  extends GeneralUser{
-    private String role;
+//    private String role;
     private String email;
     public Administrator(){}
     public Administrator(String Username, String Password, String Email){
-        this.username = Username;
-        this.password = Password;
-        this.role = "Administrator";
+        this.setUsername(Username);
+        this.setPassword(Password);
+        this.setRole("Administrator");
         this.email = Email;
 
     }
+
 }
--- app/src/main/java/com/example/meallogin/Client.java
+++ app/src/main/java/com/example/meallogin/Client.java
@@ -1,13 +1,12 @@
 package com.example.meallogin;
 
 public class Client  extends GeneralUser{
-    private String role;
     public Client(){}
     public Client(String Username, String Password, String Email){
-        this.username = Username;
-        this.password = Password;
-        this.role = "Client";
-        this.email = Email;
+        this.setUsername(Username);
+        this.setPassword(Password);
+        this.setRole("Client");
+        this.setEmail(Email);
     }
 
 }
