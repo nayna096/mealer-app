ff10ac6d4da8770fa87e1959bc83d44fe000129c
ekoro061@uottawa.ca <ekoro061@uottawa.ca>
2022-11-18 00:39:17 -0500

    added Meal class attributes and methods

--- app/src/main/java/com/example/meallogin/Meal.java
+++ app/src/main/java/com/example/meallogin/Meal.java
@@ -2,15 +2,74 @@ package com.example.meallogin;
 
 import java.io.Serializable;
 public class Meal implements Serializable{
+
+    private String name;
+    private String cuisineType;
+    private String[] ingredients;
+    private String[] allergens;
+    private double price;
     private String description;
 
+    // Constructors
     public Meal(){}
+
     public Meal(String description){
         this.description = description;
     }
 
+    public Meal(String name, String cuisineType, String[] ingredients, String[] allergens, double price, String description) {
+        this.name = name;
+        this.cuisineType = cuisineType;
+        this.ingredients = ingredients;
+        this.allergens = allergens;
+        this.price = price;
+        this.description = description;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getCuisineType() {
+        return cuisineType;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public String[] getAllergens() {
+        return allergens;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    // Setters
+    public void setName(String name) { // TODO Lisa - should visibility be private for all setters?
+        this.name = name;
+    }
+
+    public void setCuisineType(String cuisineType) {
+        this.cuisineType = cuisineType;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setAllergens(String[] allergens) {
+        this.allergens = allergens;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public void setDescription(String description) {
