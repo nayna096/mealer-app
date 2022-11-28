415906ab1629d0aa8f38b88bb2cfb75888bcd9ca
IsamKarroum <ikarr050@uottawa.ca>
2022-11-27 19:06:18 -0500

    Clients can now Search for meals

---
+++ app/src/main/java/com/example/meallogin/MealSearchAdapter.java
@@ -0,0 +1,36 @@
+package com.example.meallogin;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class MealSearchAdapter extends ArrayAdapter<Meal> {
+    public MealSearchAdapter(@NonNull Context context, ArrayList<Meal> mealList) {
+        super(context, R.layout.meal_list_item, mealList);
+    }
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+            Meal meal = getItem(position);
+
+                if (convertView == null) {
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.meal_list_item, parent, false);
+                }
+
+                TextView name = convertView.findViewById(R.id.chefName);
+                name.setText(meal.getName());
+
+                TextView description = convertView.findViewById(R.id.mealDescription);
+                description.setText(meal.getDescription());
+            return super.getView(position, convertView, parent);
+        }
+        }
+
+
--- app/src/main/java/com/example/meallogin/SearchMeals.java
+++ app/src/main/java/com/example/meallogin/SearchMeals.java
@@ -2,13 +2,67 @@ package com.example.meallogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.meallogin.databinding.ActivityComplaintsfragBinding;
+import com.example.meallogin.databinding.ActivitySearchMealsBinding;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+
+import java.util.ArrayList;
 
 public class SearchMeals extends AppCompatActivity {
+
+    ComplaintAdapter complaintAdapter;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference dbref = db.getReference();
+    ActivitySearchMealsBinding binding;
+    EditText search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_meals);
+        binding = ActivitySearchMealsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        search = findViewById(R.id.searchMeals);
+        DatabaseReference complaintsRef = db.getReference().child("Cooks").child("Menu").child("offered");
+        complaintsRef.get().addOnCompleteListener(task -> {
+            //Allows for binding to be displayed while reading from database
+            if(task.isSuccessful()){
+                GenericTypeIndicator<ArrayList<Meal>> t = new GenericTypeIndicator<ArrayList<Meal>>(){};
+                ArrayList<Meal> meals = new ArrayList<Meal>();
+                for(DataSnapshot ds: task.getResult().getChildren()){
+                    Meal c = ds.getValue(Meal.class);
+                    meals.add(c);
+                    //add outstanding complaints to list to be dealt with
+                }
+                MealSearchAdapter mealSearchAdapter = new MealSearchAdapter(SearchMeals.this, meals);
+                binding.mealTable.setAdapter(mealSearchAdapter);
+                //future code written here
+            }
+
+        });
+        MaterialButton home = (MaterialButton) findViewById(R.id.Home);
+        home.setOnClickListener(v -> {
+            openWelcomeAdminScreen();
+        });
+        MaterialButton settings = (MaterialButton) findViewById(R.id.settings);
+        settings.setOnClickListener(v -> {
+            openSettingsFrag();
+        });
     }
-}
+    public void openWelcomeAdminScreen() {
+        Intent intent = new Intent(this, WelcomeAdminScreen.class);
+        startActivity(intent);
+    }
+    public void openSettingsFrag() {
+        Intent intent = new Intent(this, SettingsFrag.class);
+        startActivity(intent);
+    }
+    }
+
