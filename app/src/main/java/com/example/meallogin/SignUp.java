9106f51ccb85fe1aa32c2234f190034965b8f1ef
nayna096 <noahta1@bell.net>
2023-02-04 11:43:32 -0500

    Basic Setup
    Got set up on Mac, app works the same.

--- app/src/main/java/com/example/meallogin/SignUp.java
+++ app/src/main/java/com/example/meallogin/SignUp.java
@@ -19,7 +19,7 @@ import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
 
-    String [] restrictedUsers =  {"nayna","gdupu","ikarr","ekoro"};
+    String restrictedUsers = "nayna";
     FirebaseDatabase db = FirebaseDatabase.getInstance();
     DatabaseReference dbref = db.getReference();
 
@@ -37,26 +37,22 @@ public class SignUp extends AppCompatActivity {
         MaterialButton cook = (MaterialButton) findViewById(R.id.Cook);
         MaterialButton client = (MaterialButton) findViewById(R.id.Client);
 
-        for(int i = 0;i<restrictedUsers.length;i++){
-            if(user.getText().toString().equals(restrictedUsers[i])){
-                Toast.makeText(getApplicationContext(),"This username is invalid due to an admin conflict",Toast.LENGTH_LONG).show();
-                user.setText("");
-            }
+        if (user.getText().toString().equals(restrictedUsers)) {
+            Toast.makeText(getApplicationContext(), "This username is invalid due to an admin conflict", Toast.LENGTH_LONG).show();
+            user.setText("");
         }
         login.setOnClickListener(v -> openMainActivity());
         client.setOnClickListener(v ->
         {
-            for(int i = 0;i<restrictedUsers.length;i++){
-                if(user.getText().toString().equals(restrictedUsers[i])){
-                    Toast.makeText(getApplicationContext(),"This username is invalid due to an admin conflict",Toast.LENGTH_LONG).show();
-                    user.setText("");
-                }
+            if (user.getText().toString().equals(restrictedUsers)) {
+                Toast.makeText(getApplicationContext(), "This username is invalid due to an admin conflict", Toast.LENGTH_LONG).show();
+                user.setText("");
             }
             dbref.child("Cooks").orderByChild("username").equalTo(user.getText().toString().trim()).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.exists()){
-                        Toast.makeText(getApplicationContext(),"This username is already used by a cook, use a different one", Toast.LENGTH_LONG).show();
+                    if (snapshot.exists()) {
+                        Toast.makeText(getApplicationContext(), "This username is already used by a cook, use a different one", Toast.LENGTH_LONG).show();
                         user.setText("");
                     }
                 }
@@ -69,11 +65,11 @@ public class SignUp extends AppCompatActivity {
             dbref.child("Clients").orderByChild("username").equalTo(user.getText().toString().trim()).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot datasnapshot) {
-                    if(datasnapshot.exists()){
-                        Toast.makeText(getApplicationContext(),"This username is already used by a client, use a different one", Toast.LENGTH_LONG).show();
+                    if (datasnapshot.exists()) {
+                        Toast.makeText(getApplicationContext(), "This username is already used by a client, use a different one", Toast.LENGTH_LONG).show();
                         user.setText("");
-                    }else{
-                        createClient(user,Password,Confirmp,Email,Address);
+                    } else {
+                        createClient(user, Password, Confirmp, Email, Address);
                     }
                 }
 
@@ -87,16 +83,14 @@ public class SignUp extends AppCompatActivity {
         });
         cook.setOnClickListener(v ->
         {
-            for(int i = 0;i<restrictedUsers.length;i++){
-                if(user.getText().toString().equals(restrictedUsers[i])){
-                    Toast.makeText(getApplicationContext(),"This username is invalid due to an admin conflict",Toast.LENGTH_LONG).show();
-                    user.setText("");
-                }
+            if (user.getText().toString().equals(restrictedUsers)) {
+                Toast.makeText(getApplicationContext(), "This username is invalid due to an admin conflict", Toast.LENGTH_LONG).show();
+                user.setText("");
             }
             dbref.child("Clients").orderByChild("username").equalTo(user.getText().toString().trim()).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.exists()) {
+                    if (snapshot.exists()) {
                         Toast.makeText(getApplicationContext(), "This username is already used by a client, use a different one", Toast.LENGTH_LONG).show();
                         user.setText("");
                     }
@@ -110,10 +104,10 @@ public class SignUp extends AppCompatActivity {
             dbref.child("Cooks").orderByChild("username").equalTo(user.getText().toString().trim()).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot datasnapshot) {
-                    if(datasnapshot.exists()){
-                        Toast.makeText(getApplicationContext(),"This username is already used by a cook, use a different one", Toast.LENGTH_LONG).show();
+                    if (datasnapshot.exists()) {
+                        Toast.makeText(getApplicationContext(), "This username is already used by a cook, use a different one", Toast.LENGTH_LONG).show();
                         user.setText("");
-                    }else{
+                    } else {
                         createCook(user, Password, Confirmp, Email, Address);
                     }
                 }
@@ -128,7 +122,7 @@ public class SignUp extends AppCompatActivity {
 
     }
 
-    public void createClient(EditText user, EditText Password, EditText Confirmp, EditText Email, EditText address){
+    public void createClient(EditText user, EditText Password, EditText Confirmp, EditText Email, EditText address) {
         if (!(TextUtils.isEmpty(user.getText().toString()) || TextUtils.isEmpty(Password.getText().toString()) || TextUtils.isEmpty(address.getText().toString()) || TextUtils.isEmpty(Email.getText().toString()))) {
             if (Confirmp.getText().toString().trim().equals(Password.getText().toString().trim())) {
                 Client newclient = new Client(user.getText().toString(), Password.getText().toString(), Email.getText().toString(), address.getText().toString());
@@ -143,21 +137,23 @@ public class SignUp extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "One of the fields is empty", Toast.LENGTH_LONG).show();
         }
     }
-    public void createCook(EditText user, EditText Password, EditText Confirmp, EditText Email, EditText address){
-        if (!(TextUtils.isEmpty(user.getText().toString()) || TextUtils.isEmpty(Password.getText().toString()) || TextUtils.isEmpty(address.getText().toString()) ||  TextUtils.isEmpty(Email.getText().toString()))) {
+
+    public void createCook(EditText user, EditText Password, EditText Confirmp, EditText Email, EditText address) {
+        if (!(TextUtils.isEmpty(user.getText().toString()) || TextUtils.isEmpty(Password.getText().toString()) || TextUtils.isEmpty(address.getText().toString()) || TextUtils.isEmpty(Email.getText().toString()))) {
             if (Confirmp.getText().toString().trim().equals(Password.getText().toString().trim())) {
                 Cook newcook = new Cook(user.getText().toString(), Password.getText().toString(), Email.getText().toString(), address.getText().toString());
                 DatabaseReference cookref = db.getReference("Cooks");
                 String id = cookref.push().getKey();
                 cookref.child(id).setValue(newcook);
                 openWelcomeCookScreen(newcook);
-            }else{
+            } else {
                 Toast.makeText(getApplicationContext(), "Your password does not match", Toast.LENGTH_LONG).show();
             }
         } else {
             Toast.makeText(getApplicationContext(), "One of the fields is empty", Toast.LENGTH_LONG).show();
         }
     }
+
     public void openMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -171,7 +167,7 @@ public class SignUp extends AppCompatActivity {
 
     public void openWelcomeClientScreen(Client c) {
         Intent intent = new Intent(this, WelcomeClientScreen.class);
-        intent.putExtra("Client",c);
+        intent.putExtra("Client", c);
         startActivity(intent);
     }
 
